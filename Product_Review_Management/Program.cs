fba3431b9de4ddd28168e296461ea62e03714e1e
Tushar Anand <tusharranand@gmail.com>
2022-04-30 14:28:38 +0530

    Introduction

---
+++ Product_Review_Management/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+namespace Product_Review_Management
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Product Review Management using LINQ");
+        }
+    }
+}
